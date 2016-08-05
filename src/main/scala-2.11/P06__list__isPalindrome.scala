7a60e5a26c9fafc565c5374ccd3efc8383f52841
Sungjun, Kim <withsmilo@gmail.com>
2016-08-05 16:34:29 +0900

    Solved P06__list__isPalindrome

---
+++ src/main/scala-2.11/P06__list__isPalindrome.scala
@@ -0,0 +1,33 @@
+
+/*
+P06 (*) Find out whether a list is a palindrome.
+Example:
+scala> isPalindrome(List(1, 2, 3, 2, 1))
+res0: Boolean = true
+ */
+
+object P06__list__isPalindrome extends App {
+
+  // Define isPalindrome method.
+  def isPalindrome[A](ls: List[A]) : Boolean = {
+    ls match {
+      case Nil => true
+      case _ :: Nil => true
+      case all => (all.head == all.last) && isPalindrome(all.tail.init)
+    }
+  }
+
+  // Test it.
+  val testList1 = List(1, 2, 3, 2, 1)
+  val testList2 = List(1, 2, 3, 2, 1, 5)
+  println("test list: " + testList1)
+  println("is palindrome?: " + isPalindrome(testList1))
+  println("test list: " + testList2)
+  println("is palindrome?: " + isPalindrome(testList2))
+  /*
+  test list: List(1, 2, 3, 2, 1)
+  is palindrome?: true
+  test list: List(1, 2, 3, 2, 1, 5)
+  is palindrome?: false
+   */
+}
