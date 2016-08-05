0180813d0a3e6b3b3e0d2239036660dcde072ac8
Sungjun, Kim <withsmilo@gmail.com>
2016-08-05 18:31:14 +0900

    Solved P08__list__compress

---
+++ src/main/scala-2.11/P08__list__compress.scala
@@ -0,0 +1,48 @@
+
+/*
+P08 (**) Eliminate consecutive duplicates of list elements.
+If a list contains repeated elements they should be replaced with a single copy of the element.
+The order of the elements should not be changed.
+Example:
+
+scala> compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
+ */
+
+object P08__list__compress extends App {
+
+  // Define compress method.
+  def compress[A](ls: List[A]): List[A] = {
+    ls.foldLeft(
+      List[A]()) { // start value
+        (m, n) =>
+          //println("  [foldLeft] m[" + m + "] n[" + n + "]");
+          if (m.isEmpty) List(n)
+          else if (m.last != n) m ::: List(n)
+          else m
+    }
+  }
+
+  // Test it.
+  val testList = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+  println("test list: " + testList)
+  println("compressed list: " + compress(testList))
+  /*
+  test list: List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    [foldLeft] m[List()] n['a]
+    [foldLeft] m[List('a)] n['a]
+    [foldLeft] m[List('a)] n['a]
+    [foldLeft] m[List('a)] n['a]
+    [foldLeft] m[List('a)] n['b]
+    [foldLeft] m[List('a, 'b)] n['c]
+    [foldLeft] m[List('a, 'b, 'c)] n['c]
+    [foldLeft] m[List('a, 'b, 'c)] n['a]
+    [foldLeft] m[List('a, 'b, 'c, 'a)] n['a]
+    [foldLeft] m[List('a, 'b, 'c, 'a)] n['d]
+    [foldLeft] m[List('a, 'b, 'c, 'a, 'd)] n['e]
+    [foldLeft] m[List('a, 'b, 'c, 'a, 'd, 'e)] n['e]
+    [foldLeft] m[List('a, 'b, 'c, 'a, 'd, 'e)] n['e]
+    [foldLeft] m[List('a, 'b, 'c, 'a, 'd, 'e)] n['e]
+  compressed list: List('a, 'b, 'c, 'a, 'd, 'e)
+   */
+}
