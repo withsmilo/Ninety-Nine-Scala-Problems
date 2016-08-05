96187bf443f09a11d1fc48c3a15c675212df1693
Sungjun, Kim <withsmilo@gmail.com>
2016-08-05 11:24:45 +0900

    Solved P03

---
+++ src/main/scala-2.11/P03.scala
@@ -0,0 +1,36 @@
+
+/*
+P03 (*) Find the Kth element of a list.
+By convention, the first element in the list is element 0.
+Example:
+
+scala> nth(2, List(1, 1, 2, 3, 5, 8))
+res0: Int = 2
+ */
+
+object P03 extends App {
+
+  // Define nth method.
+  def nth[A](kth: Int, ls: List[A]): A = {
+    (kth, ls) match {
+      case (0, item :: _ ) => item
+      case (i, _ :: remainder) => nth(i - 1, remainder)
+      case (_, Nil) => throw new NoSuchElementException
+    }
+  }
+
+  // Test it.
+  val testList = List(1, 1, 2, 3, 5, 8)
+  println("test list: " + testList)
+  println("2th element: " + nth(2, testList))
+  /*
+   1. nth(2, List(1, 1, 2, 3, 5, 8))
+   2. nth(1, List(1, 2, 3, 5, 8))
+   3. nth(0, List(2, 3, 5, 8))
+   4. 2
+   */
+  /*
+  test list: List(1, 1, 2, 3, 5, 8)
+  2th element: 2
+   */
+}
