94b83827dbc90ad409451efe13bd4d032e687291
Sungjun, Kim <withsmilo@gmail.com>
2016-08-25 10:21:24 +0900

    Update P01__list__last

--- src/main/scala-2.11/P01__list__last.scala
+++ src/main/scala-2.11/P01__list__last.scala
@@ -9,9 +9,11 @@ res0: Int = 8
 import scala.annotation.tailrec
 
 object P01__list__last extends App {
-  val testList1 = List()
-  val testList2 = List(9)
-  val testList3 = List(1, 1, 2, 3, 5, 8)
+
+  def test(idx: Int, fn: List[Int] => Int) {
+    val l = List(1, 1, 2, 3, 5, 8)
+    println(s"[#$idx] Input:$l, last(List) result:" + fn(l))
+  }
 
   // #1 Use recursive method
   @tailrec def last1[A](ls: List[A]): A = {
@@ -21,19 +23,21 @@ object P01__list__last extends App {
       case _ => throw new NoSuchElementException
     }
   }
-  //println("[#1] Input[" + testList1 + "], last[" + last1(testList1) + "]")
-  println("[#1] Input[" + testList2 + "], last[" + last1(testList2) + "]")
-  println("[#1] Input[" + testList3 + "], last[" + last1(testList3) + "]")
+  test(1, last1)
 
   // #2 Use 'fold' method
-  def last2[A](ls: List[A]) = ls.fold(None: Option[A])((x, y) => y)
-  println("[#2] Input[" + testList1 + "], last[" + last2(testList1) + "]")
-  println("[#2] Input[" + testList2 + "], last[" + last2(testList2) + "]")
-  println("[#2] Input[" + testList3 + "], last[" + last2(testList3) + "]")
+  def last2[A](ls: List[A]): A = {
+    ls.fold(None: Option[A])((x, y) => Some(y)) match {
+      case Some(item: A) => item
+      case _ => throw new NoSuchElementException
+    }
+  }
+  test(2, last2)
 
   // #3 Use 'foldLeft(/:)' method
-  def last3[A](ls: List[A]) = ((None: Option[A]) /: ls)((x, y) => Some(y))
-  println("[#3] Input[" + testList1 + "], last[" + last3(testList1).getOrElse(None) + "]")
-  println("[#3] Input[" + testList2 + "], last[" + last3(testList2).getOrElse(None) + "]")
-  println("[#3] Input[" + testList3 + "], last[" + last3(testList3).getOrElse(None) + "]")
+  def last3[A](ls: List[A]): A = {
+    ((None: Option[A]) /: ls)((x, y) => Some(y))
+      .getOrElse(throw new NoSuchElementException)
+  }
+  test(3, last3)
 }
